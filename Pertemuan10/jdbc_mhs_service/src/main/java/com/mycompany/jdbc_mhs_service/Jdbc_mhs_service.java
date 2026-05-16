947cb95698a854760ec2d5b363f4a6f19ba94f43
ilhamraffi <ilhamraffi882@gmail.com>
2026-05-16 14:01:55 +0700

    Pertemuan10

---
+++ Pertemuan10/jdbc_mhs_service/src/main/java/com/mycompany/jdbc_mhs_service/Jdbc_mhs_service.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.jdbc_mhs_service;
+
+/**
+ *
+ * @author Acer
+ */
+public class Jdbc_mhs_service {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
